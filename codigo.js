41c3efaea42f17fd13a648ef02ce234ca4749cb2
brinkz <isabel.sbg11@gmail.com>
2022-07-19 18:38:07 -0600

    modificacion en js

--- codigo.js
+++ codigo.js
@@ -1,6 +1,3 @@
-<script type="text/javascript">
-        var ruta = window.location;
-        
-        console.log(ruta);
-        document.write("Estas en: " + ruta);
-</script>
+var ruta = window.location;     
+console.log(ruta);
+document.write("Estas en: " + ruta);
